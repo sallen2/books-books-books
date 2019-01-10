aae703b56d1c60f0bbf7518f3af81fa75a0d24f1
Stephan Allen <sallen202009@gmail.com>
2019-01-10 01:18:03 -0500

    added axios

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const routes = require('./routes/index')
 const app = express()
+const axios = require('axios')
 const mongoose = require('mongoose')
+
 
 const PORT = process.env.PORT || 3001
 
@@ -11,6 +14,23 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('books/build'))
 }
+
+const search = "zelda"
+axios({
+    url: "https://api-v3.igdb.com/search",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'user-key': '67e084e0ae7fc93c4a2f97f0e8616844'
+    },
+    data: `search "${search}"; fields game.name, game.url, game.storyline, game.genres.name;`
+  })
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 app.use(routes)
 
